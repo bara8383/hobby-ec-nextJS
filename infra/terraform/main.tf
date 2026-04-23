7c980a6872639fece154b17c3ba15a724a7c2190
bara8383 <130632770+bara8383@users.noreply.github.com>
2026-04-24 01:18:34 +0900

    terraformの修正 by vscode

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -14,7 +14,7 @@ data "aws_subnets" "default" {
 }
 
 data "aws_ssm_parameter" "ecs_optimized_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id"
 }
 
 locals {
